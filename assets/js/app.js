e16c76882ab0b429f196e02884657684664b7ab0
kurshot <sonkurahmetkursat@gmail.com>
2022-06-12 02:03:47 +0300

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,11 +3,22 @@ let acc = document.querySelectorAll('.btn')
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
       this.classList.toggle("active");
-      var panel = this.nextElementSibling;
-      if (panel.style.display === "block") {
-        panel.style.display = "none";
-      } else {
-        panel.style.display = "block";
+
+      var icon = this.firstElementChild;
+      var parentDiv = this.parentElement;
+
+      if(!parentDiv.classList.contains('active')){
+        icon.classList.remove('fa-circle-plus');
+        icon.classList.add('fa-circle-minus');
+        parentDiv.classList.add('active');
       }
+      else{
+        icon.classList.remove('fa-circle-minus');
+        icon.classList.add('fa-circle-plus');
+        parentDiv.classList.remove('active');
+      }
+
+
+
     });
   }
